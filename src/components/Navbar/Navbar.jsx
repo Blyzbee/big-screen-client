71ff11fe09f7c165462ef635e2930275a94702ab
NateDev <jonathanmbaya13@gmail.com>
2023-12-12 10:58:10 +0100

    Login part 2 (avec Logout bouton navbar)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,32 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 import Logo from "../../assets/Logo.png";
 import Button from "../Button/Button";
 import Icon from "../Icon/Icon";
 import "./navbar.scss";
+import {AccountAuth} from "../../services/AccountAuth"
 
 const Navbar = () => {
 	const token = localStorage.getItem("token");
+	const navigate = useNavigate(); 
+
+
+	const logout = () => {
+		AccountAuth.logout()
+		.then((res) => {console.log(res);
+		navigate('/admin');
+		})
+
+		.catch((error) => {
+		console.error('Logout error:', error);
+		});
+	};
 
 	return (
 		<nav>
 			<img src={Logo} alt="logo Big Screen" />
 			{token ? (
-				<Button>
+				<Button onClick={logout}>
 					Deconnexion <Icon name="logout" mx />
 				</Button>
 			) : (
--- src/views/Navbar/Navbar.jsx
+++ src/views/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './Navbar.css'
+import { useNavigate } from 'react-router-dom';
+import Button from "../../components/Button/Button";
+import Icon from "../../components/Icon/Icon";
+import {AccountAuth} from "../../services/AccountAuth"
+import './Navbar.scss'
 
 const Navbar = () => {
+	const navigate = useNavigate(); 
+
+
+	const logout = () => {
+		AccountAuth.logout()
+
+		navigate('/admin');
+	};
     return (
-        <div className='main-admin-container'>
+        <div className='navbar-area main-admin-container'>
             <nav>
                 
                 <h2>BigScreen</h2>
@@ -14,6 +26,16 @@ const Navbar = () => {
                     <li><NavLink to="/admin/questions" activeClassName="active-link">Questionnaires</NavLink></li>
                     <li><NavLink to="/admin/answers" activeClassName="active-link">Réponses</NavLink></li>
                 </ul>
+
+                {AccountAuth.isLogged ? (
+				<Button onClick={logout}>
+					Deconnexion <Icon name="logout" mx />
+				</Button>
+			) : (
+				<Button>
+					Connexion <Icon name="login" mx />
+				</Button>
+			)}
             </nav>
         </div>
     );
