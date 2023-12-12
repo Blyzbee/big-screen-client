c957ff7898fd84c3de6e21df97774f1c7d21eb7b
Dorian URBANIAK <dorian.urbaniak@gmail.com>
2023-12-12 09:34:12 +0100

    navbar done

--- src/components/Icon.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Icon = ({ name }) => {
-	return <span class="material-symbols-outlined">{name}</span>;
-};
-
-export default Icon;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Logo from "../../assets/Logo.png";
 import Button from "../Button/Button";
+import Icon from "../Icon/Icon";
+import "./navbar.scss";
 
 const Navbar = () => {
+	const token = localStorage.getItem("token");
+
 	return (
 		<nav>
 			<img src={Logo} alt="logo Big Screen" />
-			<Button>Deconnexion</Button>
+			{token ? (
+				<Button>
+					Deconnexion <Icon name="logout" mx />
+				</Button>
+			) : (
+				<Button>
+					Connexion <Icon name="login" mx />
+				</Button>
+			)}
 		</nav>
 	);
 };
