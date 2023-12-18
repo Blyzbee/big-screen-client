e981e9b87c04a6a16746ca2a63a3b60d1ccbbc66
NateDev <jonathanmbaya13@gmail.com>
2023-12-18 19:43:07 +0100

    Pop Up et styles

--- src/views/Survey/Survey.jsx
+++ src/views/Survey/Survey.jsx
@@ -3,6 +3,7 @@ import "./survey.scss";
 import Button from "../../components/Button/Button";
 import { getQuestions, sendAnswers } from "../../services/Api";
 import Loading from "../../components/Loading/Loading";
+import ReactModal from "react-modal";
 
 const Survey = () => {
 	const [formData, setFormData] = useState({});
@@ -10,6 +11,12 @@ const Survey = () => {
 	const [screenWidth, setscreenWidth] = useState(0);
 	const [errorMsg, setErrorMsg] = useState("");
 	const [questions, setQuestions] = useState(null);
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const openModal = () => {
+		setIsModalOpen(true);
+	};
+	
 
 	const handleChange = (id, value) => {
 		setFormData({ ...formData, [id]: value });
@@ -29,7 +36,10 @@ const Survey = () => {
 		}
 
 		sendAnswers(formData)
-			.then((res) => console.log(res))
+			.then((res) => {
+				console.log(res);
+				openModal(); // Ouvrez le pop-up après la validation réussie
+			})
 			.catch((err) => console.log(err));
 	};
 
@@ -60,56 +70,71 @@ const Survey = () => {
 
 	if (!questions) return <Loading />;
 	return (
-		<form className="survey" onSubmit={handleSubmit}>
 
-			<div className="carousel">
-			<h2>Questionnaire</h2>
-			<p>Veuillez à bien remplir tous les champs du formulaire.</p>
-				<div style={{ marginLeft: `${-100 * (step - 1)}dvw` }}>
-					{questions &&
-						questions.map((question) => (
-							<div key={question.id}>
-								<label>{`${question.id}/20 | ${question.body}`}</label>
-								{question.type === "A" && (
-									<select
-										onChange={(e) => handleChange(question.id, e.target.value)}
-									>
-										<option value="">Sélectionnez une option</option>
-										{JSON.parse(question.choices).map((option, index) => (
-											<option key={index} value={option}>
-												{option}
-											</option>
-										))}
-									</select>
-								)}
-								{question.type === "B" && (
-									<input
-										type="text"
-										onChange={(e) => handleChange(question.id, e.target.value)}
-										maxLength={255}
-									/>
-								)}
-								{question.type === "C" && (
-									<input
-										type="number"
-										onChange={(e) => handleChange(question.id, e.target.value)}
-										min={1}
-										max={5}
-									/>
-								)}
-							</div>
-						))}
+		<div>
+			<ReactModal
+				isOpen={isModalOpen}
+				onRequestClose={() => setIsModalOpen(false)}
+                contentLabel="Voir réponse a été enregistré"
+            >
+                {/* Ajoutez le contenu personnalisé ici */}
+                <h2>Merci ! <br></br> Votre réponse a bien été enregistrée</h2>
+			</ReactModal>
+
+
+			<form className="survey" onSubmit={handleSubmit}>
+
+				<div className="carousel">
+				<h2>Questionnaire</h2>
+				<p>Veuillez à bien remplir tous les champs du formulaire.</p>
+					<div style={{ marginLeft: `${-100 * (step - 1)}dvw` }}>
+						{questions &&
+							questions.map((question) => (
+								<div key={question.id}>
+									<label>{`${question.id}/20 | ${question.body}`}</label>
+									{question.type === "A" && (
+										<select
+											onChange={(e) => handleChange(question.id, e.target.value)}
+										>
+											<option value="">Sélectionnez une option</option>
+											{JSON.parse(question.choices).map((option, index) => (
+												<option key={index} value={option}>
+													{option}
+												</option>
+											))}
+										</select>
+									)}
+									{question.type === "B" && (
+										<input
+											type="text"
+											onChange={(e) => handleChange(question.id, e.target.value)}
+											maxLength={255}
+										/>
+									)}
+									{question.type === "C" && (
+										<input
+											type="number"
+											onChange={(e) => handleChange(question.id, e.target.value)}
+											min={1}
+											max={5}
+										/>
+									)}
+								</div>
+							))}
+					</div>
 				</div>
-			</div>
-			<div className="submit-button">
-				{errorMsg && <span className="text-error">{errorMsg}</span>}
-				{step >= 20 || screenWidth > 1120 ? (
-					<Button type="submit">Valider le formulaire</Button>
-				) : (
-					<Button type="submit">Suivant</Button>
-				)}
-			</div>
-		</form>
+				<div className="submit-button">
+					{errorMsg && <span className="text-error">{errorMsg}</span>}
+					{step >= 20 || screenWidth > 1120 ? (
+						<Button type="submit">Valider le formulaire</Button>
+					) : (
+						<Button type="submit">Suivant</Button>
+					)}
+				</div>
+			</form>
+
+		</div>
+		
 	);
 };
 
